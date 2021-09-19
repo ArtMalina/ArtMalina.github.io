ff1d2d8b8fce305642a8061f6288649193b6c201
Artem Malinovskii <amalinovskiy@avanpost.ru>
2021-09-19 17:22:15 +0300

    built prohect folder with start dummy index

---
+++ dist/precache-manifest.a2b0ac65d5b0be4219e570a625f1bf21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa2efc51f21dd5aa2a512bf5e51dde5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e3518e03376ecb8beb6",
+    "url": "/static/css/main.594acec7.chunk.css"
+  },
+  {
+    "revision": "413d350d0a3001dfe437",
+    "url": "/static/js/2.6cc391f2.chunk.js"
+  },
+  {
+    "revision": "0e3518e03376ecb8beb6",
+    "url": "/static/js/main.a2ed35b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
